9fafadb126f02e3ba7321cbf861e10c858491e9b
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2024-10-07 18:42:45 +0200

    initial

---
+++ 02/simple.hs
@@ -0,0 +1,64 @@
+-- This is the simple program from the slides from the introduction
+
+laengde :: (Num p) => [a] -> p
+
+laengde [] = 0
+laengde (x:l) = 1 + (laengde l)
+
+myList = [2,3,17,9,69,484000]
+
+data BTree = BLeaf Int | BBranch Int BTree BTree deriving Show
+
+-- sumtree :: BTree -> Int
+
+sumtree (BLeaf x) = x
+sumtree (BBranch x t1 t2) = let v1 = sumtree t1
+                                v2 = sumtree t2
+                            in x + v1 + v2
+
+
+myBigOak = BBranch 14 (BLeaf 13) (BLeaf 17)
+
+-- Quicksort
+
+qsort :: (Ord a) => [a] -> [a]
+
+qsort [] = []
+qsort (x:xs) = small ++ [x] ++ big
+                 where small = qsort [a | a <- xs, a <= x]
+                       big   = qsort [a | a <- xs, a > x]
+
+
+-- partial: not defined for lists with size <2
+second :: [a] -> a
+second x = x !! 1 
+
+-- total: defined for lists of any size
+second' :: [a] -> Maybe a
+second' x | laengde x >=2 = Just (x !! 1)
+          | otherwise = Nothing
+
+allbutsecond :: [a] -> [a]
+
+-- allbutsecond (x:y:z:xs) = x:xs
+allbutsecond (x:y:xs) = x:xs
+
+allbutsecond' :: [a] -> [a]
+allbutsecond' xs = y:z
+                where y = head xs
+                      z = tail (tail xs)
+
+midtover :: [a] -> ([a],[a])
+midtover x = (a,b)
+            where a = take (laengde x `div` 2) x
+                  b = drop (laengde x `div` 2) x
+
+-- midtover' :: [a] -> ([a],[a])
+-- midtover' [] = ([],[])
+-- midtover' [x] ([],[x])
+-- midtover' [x,y] ([x],[y])
+-- midtover' (x:xs) =  (x:y,z:zs)
+--                 where mid =
+--                       y = 
+--                       z = 
+--                       zs = last xs
---
+++ 03/prep.hs
@@ -0,0 +1,84 @@
+-- quango :: a -> [ a ]
+
+-- tango :: Num p1 => ( a , b ) -> p2 -> p1
+
+-- 1. opgave
+
+{- 
+    a function that takes a value of type a and produces a value of same type.
+    second parameter is a value of type a. 
+    the entire function returns a value of type a 
+-}
+-- twice :: (a -> a) -> a -> a 
+-- twice f x = f ( f ( x ) ) 
+
+-- twice :: a -> b -> b 
+
+-- 1.2
+
+{-
+    twicetwo takes a pair
+        first element is a function taking a value of type a and returning a value of type a
+        second element is a value of type a
+    The function returns a value of type a
+-}
+
+
+-- twicetwo :: ((a -> a),a) -> a
+-- twicetwo (f,x) = f (f (x))
+
+
+-- 3. opgave
+
+{- 
+    takes a tuple (pair) of values of type a
+    returns a list of a's
+ -}
+
+-- dingo :: (a,a) -> [a]
+-- dingo ( x , y ) = [ x , y ]
+
+
+-- 4. opgave
+
+{- 
+    functions cannot be Eq, since it is (generally) impossible to test if a pair of functions produce the same values for every input
+-}
+
+-- a. opgave
+
+-- mango :: Num a => a -> a -> a -> a
+-- mango x y z = x * y + z - 42
+-- mango 14 :: Int -> Int -> Int
+
+-- b. opgave
+
+-- bingo :: a -> a
+
+-- c. opgave
+
+-- thesame :: Eq a => [(a,a)] -> [(a,a)]
+
+
+-- d. opgave
+
+-- aka. a list of binary infix functions (operators)
+-- [ (+) , (*) , (+) , (-) ] :: [(a -> a -> a)]
+-- [ (+) , (*) , (+) , (-), (++) ] :: Exception (virker åbenbart)
+
+-- e. opgave
+
+-- mymap :: (a -> b) -> [a] -> [b] 
+-- mymap f xs = 
+
+-- f. opgave
+
+somefunc :: (Ord a1, Eq a2) =>a2 -> a2 -> (a1, a1)-> a1
+somefunc a b (x,y) = if a == b then x else y
+
+-- g. opgave
+
+--madras (f,x,y) = f (f x x) y
+
+-- madras' :: (t -> t -> t) -> t -> t -> t
+madras' f x y = f (f x x) y
