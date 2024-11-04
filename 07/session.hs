3bc849778f6e5230bf38d0bab09b0055b8391188
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2024-11-04 15:32:17 +0100

    post cleanup

--- 07/session.hs
+++ 07/session.hs
@@ -49,16 +49,6 @@ values :: Encyclopedia a -> [a]
 values (Leaf' _ v) = [v]
 values (Node' _ v vs) = v : concatMap values vs
 
--- maxVal :: (Ord a) => Encyclopedia a -> a
--- maxVal (Leaf' _ v) = v
--- maxVal (Node' _ v vs) = maximum (v : concatMap values vs)
-
-minLayer :: (Ord a) => [Encyclopedia a] -> a
-minLayer [Leaf' _ v] = v
-minLayer [Node' _ v _] = v
-minLayer ((Leaf' _ v) : es) = min v (minLayer es)
-minLayer ((Node' _ v vs) : es) = min v (minLayer es)
-
 layer :: [Encyclopedia a] -> [a]
 layer [Leaf' _ v] = [v]
 layer [Node' _ v _] = [v]
@@ -68,52 +58,23 @@ layer ((Node' _ v vs) : es) = v : layer es
 children :: Encyclopedia a -> [a]
 children (Leaf' _ _) = []
 children (Node' _ _ vs) = layer vs
--- maximum v:(map layer vs)
 
-layered :: (Ord a) => Encyclopedia a -> Bool
-layered (Leaf' _ _) = True
-layered (Node' _ x xs) = isLayered && all layered xs
-  where
-    isLayered = all (> (x)) (concatMap values xs)
-
-layered' :: (Ord a) => Encyclopedia a -> Bool
-layered' (Leaf' _ _) = True
-layered' (Node' _ x xs) = all (> x) (layer xs) && all layered' xs
-
--- layered' (Node' _ x xs) = x < minLayer xs && all layered' xs
-
-layered'' :: (Ord a) => [Encyclopedia a] -> Bool
-layered'' [Leaf' _ _] = True
-layered'' [Node' _ x es] = all (> x) (layer es) && layered'' es
-layered'' (e : es) = layered'' [e] && layered'' es
-
--- isLayered = all (all (> x) . values) xs -- aparrently the preffered way of doing this
-
--- layered''' :: Ord a => [Encyclopedia a] -> Maybe a -> Bool
--- layered''' [e] a = isNothing a || (all (\x -> x > fromJust a ) (layer [e])) && layered'''
-layered'''' :: (Ord a) => Maybe a -> Encyclopedia a -> Bool
-layered'''' a (Leaf' _ v) = maybe True (v >) a
-layered'''' a (Node' _ v es) = maybe True (v >) a && all childrenLayered es
-  where
-    childrenLayered = layered'''' (Just (maximum (layer es)))
-
+-- sjette gang er lykkens gang :))))
 layered''''' :: (Ord a) => Maybe a -> Encyclopedia a -> Bool
 layered''''' a (Leaf' _ v) = maybe True (v >) a
-layered''''' a (Node' _ v es ) = (maybe True (v >) a && cond) && all next es
+layered''''' a (Node' _ v es) = (maybe True (v >) a && cond) && all next es
   where
     maxlayer = maximum (layer es) -- will throw on node with empty list
     minGrandchildren = listToMaybe (concatMap children es)
-    cond = maybe True (maxlayer <) minGrandchildren 
+    cond = maybe True (maxlayer <) minGrandchildren
     next = layered''''' (Just v)
--- layered''''' a e = maybe True (head (layer [e])>) a 
 
--- layered'''' ((Node' _ v es),a) = all (> v) (layer es)
-
+-- should be False
 layeredNt = layered''''' Nothing (Node' "plonk" 4 [Node' "zap" 3 [Leaf' "ninka" 8], Node' "uhu" 4 [Leaf' "gif" 9], Leaf' "bingo" 5])
-
+-- should be False
 layeredNt' :: Bool
 layeredNt' = layered''''' Nothing (Node' "plonk" 1 [Node' "zap" 3 [Leaf' "ninka" 8], Node' "uhu" 4 [Leaf' "gif" 4], Leaf' "bingo" 5])
-
+-- should be False
 layertest = layered''''' Nothing (Node' "a" 1 [Node' "b" 2 [Leaf' "c" 3], Leaf' "d" 4])
 
 class InVector a where
