1afc83ab055f9112a08205c194faaa16ec8214ae
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2025-01-04 20:51:04 +0100

    previous exam sets (partially)

---
+++ eks/2022solution.hs
@@ -0,0 +1,92 @@
+-- 1.1
+
+-- 1.2
+allAnswers f xs =
+  let list = map f xs
+   in if all (/= Nothing) list
+        then Just list
+        else Nothing
+
+-- 1.3
+allAnswers' f [] = Just []
+allAnswers' f (x : xs) = do
+  fx <- f x
+  fxs <- allAnswers' f xs
+  return (fx : fxs)
+
+-- 2.1a
+a x (u, v) = if u == v then [x] else []
+
+-- 2.1b
+b f p l r = if p && l == r then f l r else False
+
+-- 2.1c
+-- c v mb = do
+--     sequence_ [show v]
+--     return mb
+
+-- 2.1d
+d f l r = [f l, f r]
+
+-- 2.2
+-- functions a,c and d are parametrically polymorphic while functions a, b and c are ad hoc polymorphic
+
+-- 3.1
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+
+figureOne = Node (Leaf "dog") (Node (Leaf "cat") (Leaf "hamster"))
+
+-- 3.2
+minimax t =
+  let list = treeToList t
+   in (minimum list, maximum list)
+  where
+    treeToList (Leaf e) = [e]
+    treeToList (Node l r) = treeToList l ++ treeToList r
+
+-- 4.1
+echo =
+  putStr "Please type a word: " >> getLine >>= \x ->
+    putStrLn ("You typed " ++ x)
+
+-- 4.2
+boolPairs i = read i::[(Bool,Bool)]
+seconds = do
+    input <- getLine
+    putStrLn $ show $ map snd $ boolPairs input
+    return ()
+-- [(True,False),(False,True),(False,False)]
+
+-- 5.1
+-- Alternating lists (using shown syntax) is disallowed since list elements in Haskell can only be of one type
+
+-- 5.2
+data Alternating a b = Pair (a,b) | Else a deriving Show
+-- The Else constructor is necessary if lists are uneven length (i.e. more a's than b's)
+-- myalt = [A 5, B True,A 6,B False, A 7, B True]
+myalt = [Pair (5,True),Pair(6,False),Pair(7,False)]
+-- 5.3
+separate [] = ([],[])
+separate ((Else l):xs) = let (left,right) = separate xs in
+    (l:left,right)
+separate ((Pair (l,r)):xs) = let (left,right) = separate xs in
+    (l:left,r:right)
+
+-- 5.4
+infinite = map (\x -> Pair (x,replicate x 'a'))[1..]
+
+newtype ToPairs a = TP (a,a)
+-- 6.1 
+booltopairs = TP (True,False)
+ftoPairs = (\x -> if x == Just 1 then 0 else 1,\y -> if y == Just 0 then 1 else 0)
+    -- where f mx = if mx == Nothing then 0 else 1 
+
+instance Functor ToPairs where
+--   fmap :: (a -> b) -> ToPairs a -> ToPairs b
+  fmap g (TP (l,r)) = TP (g l, g r)
+
+instance Applicative ToPairs where
+--   pure :: a -> ToPairs a
+  pure v = TP (v,v)
+--   (<*>) :: ToPairs (a -> b) -> ToPairs a -> ToPairs b
+  (<*>) (TP (g,_)) (TP (l,r)) = TP (g l, g r)
---
+++ eks/solutions.hs
@@ -0,0 +1,93 @@
+import Text.Printf (IsChar(toChar))
+
+-- problem 1.1
+rotate (x : xs) = xs ++ [x]
+
+-- the function is parametrically polymorhpic. No type constraints are provided, hence the function will work for lists of any type
+
+-- problem 1.2
+allrotates xs = xs : take (length xs - 1) (allrotates (rotate xs))
+
+-- This function is also parametrically polymorphic since type a can be substituted for any type
+
+-- problem 1.3
+-- allrotates' xs TBD
+
+-- problem 2.1
+data Tree a = Leaf a | Node (Tree a) (Maybe a) (Tree a) deriving (Show)
+
+-- Tree t1
+t1 = Node (Node (Leaf 17) Nothing (Leaf 484000)) Nothing (Leaf 1964)
+
+-- Tree t2
+t2 = Node (Leaf "plip") (Just "bingo") (Node (Leaf "uhu") (Just "plop") (Leaf "fedtmule"))
+
+-- Problem 2.2
+isfull (Leaf _) = True
+isfull (Node _ Nothing _) = False
+isfull (Node l _ r) = isfull l && isfull r
+
+-- Problem 2.3
+preorder (Leaf x) = Just [x]
+preorder (Node l x r) = do
+  y <- x
+  left <- preorder l
+  right <- preorder r
+  return (y : (left ++ right))
+
+-- Problem 3.1
+remove xs ys = [y | y <- ys, y `notElem` xs]
+
+-- Problem 3.2
+remove' [] ys = ys
+remove' (x : xs) ys = remove xs $ filter (/= x) ys
+
+-- Problem 4
+newtype WrapString a = WS (a,String) deriving Show
+
+instance Functor WrapString where
+    fmap :: (a -> b) -> WrapString a -> WrapString b
+    fmap f (WS (x,s)) = WS (f x,s)
+
+-- Problme 4.1
+instance Applicative WrapString where
+    pure :: a -> WrapString a
+    pure x = WS (x,"")
+    (<*>) :: WrapString (a -> b) -> WrapString a -> WrapString b
+    (<*>) (WS (g,_)) (WS (x,s)) = WS (g x,s)
+
+instance Monad WrapString where
+    return = pure
+    (>>=) :: WrapString a -> (a -> WrapString b) -> WrapString b
+    (>>=) (WS (x,s)) g = g x
+
+-- Problem 4.3    
+-- pairup :: WrapString a -> WrapString b -> WrapString (a,b)
+pairup (WS (x,s)) rws = do
+    y <- rws
+    WS ((x,y),s)
+
+-- Problem 5.1
+fivepointone x y z = if x+y<z then (x,y) else (z,y)
+-- this function is ad hoc polymorhpic, since the types of x, y and z is constrained to Ord and Num by the functions (+) and (<)
+
+-- Problem 5.2
+fivepointtwo = [(toInteger 1, \x -> 'c')]
+-- This expression is parametrically polymorphic since lambda expression accepts arguments of any type
+
+-- Problem 5.3
+fivepointthree f x = f x True 
+-- This function is parametrically polymorphic since types t1 and t2 are unconstrained
+
+-- Problem 5.4
+fivepointfour x = map (1+) [x..]
+-- This function is ad hoc polymorphic since the type x is constrained to the typeclasses Num and Enum
+
+-- Problem 6.1 TBD
+
+-- Problem 6.2
+facs = map (\x -> product [1..x]) [0..]
+
+-- Problem 6.3
+facs' = zipWith (*) facs' [1..] 
+-- facs'' = 1 : (zipWith (*) )
